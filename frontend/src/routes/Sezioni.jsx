6dc01e59c5d8aeac076d169bef0ffad576880498
Giacomo Magisano <giacomo.magisano@refresh.academy.org>
2025-07-21 01:41:52 +0200

    Frontend: Sezioni dinamiche
    Aggiunta una chiamata a backend/api/startsurvey in Bruno.
    Modificato /sezioni per fargli leggere le sezioni dal backend.
    
    TODO:
        -  inviare a backend le sezioni da compilare con l'endpoint  /api/startsurvey
        - creare l'endpoint /api/startsurvey nel backend

--- frontend/src/routes/Sezioni.jsx
+++ frontend/src/routes/Sezioni.jsx
@@ -1,26 +1,43 @@
-import * as React from 'react';
 import { Link } from "react-router";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const Checkbox = ({ label, value, onChange }) => {
+/*
+id_sezione : 1
+numero_domande : 7
+titolo : "Casa"
+*/
+const Checkbox = ({ label, count, value, isChecked, onChange }) => {
   return (
-    <label>
-      <input type="checkbox" checked={value} onChange={onChange} />
-      {label}
+    <label className="flex py-2 gap-2 items-center">
+      <input className="sezioneSelector" type="checkbox" value={value} checked={isChecked} onChange={onChange} />
+      {label} <span className="text-gray-500 text-xs">({count} domand{count==1?'a':'e'})</span>
     </label>
   );
 };
 
 const Sezioni = () => {
-  const [checkedCasa, setCheckedCasa] = React.useState(false);
-  const [checkedTrasporti, setCheckedTrasporti] = React.useState(false);
-  const [checkedViaggi, setCheckedViaggi] = React.useState(false);
-  const [checkedAlimentazione, setCheckedAlimentazione] = React.useState(false);
-  const [checkedConsumi, setCheckedConsumi] = React.useState(false);
-  const [checkedTutti, setCheckedTutti] = React.useState(false);
+  const [sezioni, setSezioni] = useState ([]);
+  useEffect(() => {
+      fetch("http://localhost:3000/api/questionario/1/sezioni")
+         .then( res => res.json())
+         .then(data => {
+              const sez= data.map((s) => { return {...s, isChecked:false}});
+              setSezioni(sez)
+          })
+         .catch(err => console.log("Sezioni Presenti error:", err));
+  },[]);
   
-  const handleChangeCasa = () => {
-    setCheckedCasa(!checkedCasa);
+  const [checkedCasa, setCheckedCasa] = useState(false);
+  const [checkedTrasporti, setCheckedTrasporti] = useState(false);
+  const [checkedViaggi, setCheckedViaggi] = useState(false);
+  const [checkedAlimentazione, setCheckedAlimentazione] = useState(false);
+  const [checkedConsumi, setCheckedConsumi] = useState(false);
+  const [checkedTutti, setCheckedTutti] = useState(false);
+  
+  const handleChangeSezione = (e) => {
+    const sez = sezioni.map(s => s.id_sezione == e.target.value?{...s, isChecked:!s.isChecked}:s)
+    setSezioni(sez);
+    console.log(JSON.stringify(sez));
   };
   const handleChangeTrasporti = () => {
     setCheckedTrasporti(!checkedTrasporti);
@@ -34,61 +51,35 @@ const Sezioni = () => {
   const handleChangeConsumi = () => {
     setCheckedConsumi(!checkedConsumi);
   };
-  const handleChangeTutti = () => {
-    setCheckedTutti(!checkedTutti);
-    setCheckedCasa(!checkedTutti);
-    setCheckedTrasporti(!checkedTutti);
-    setCheckedViaggi(!checkedTutti);
-    setCheckedAlimentazione(!checkedTutti);
-    setCheckedConsumi(!checkedTutti);
+  const handleChangeTutti = (ev) => {
+    console.log(ev.target);
+    console.log(document.querySelectorAll("input.controllo[checked=true]"))
+    document.querySelectorAll("input.sezioneSelector").forEach(i => !ev.target.checked);
   };
+
   return (
     <>
       <h1>Su che cosa vuoi fare il questionario?</h1>
-      <div className=" flex-col">
+      <div className=" flex-col py-8">
         <div className=" flex">
           <Checkbox
           className="controllo"
-            label="Tutti"
+            label="Tutto il questionatio"
             value={checkedTutti}
-            onChange={handleChangeTutti} />
+            count={(sezioni.map(s => s.numero_domande)).reduce((a,b)=>a+b, 0)}
+            onChange={handleChangeTutti}
+             />
         </div>
-        <div className=" flex">
-          <Checkbox
-          className="controllo"
-            label="Casa"
-            name="Casa"
-            value={checkedCasa}
-            onChange={handleChangeCasa} />
-        </div>
-        <div className=" flex">
-          <Checkbox
-          className="controllo"
-            label="Trasporti"
-            value={checkedTrasporti}
-            onChange={handleChangeTrasporti} />
-        </div>
-        <div className=" flex">
-          <Checkbox
-          className="controllo"
-            label="Viaggi"
-            value={checkedViaggi}
-            onChange={handleChangeViaggi} />
-        </div>
-        <div className=" flex">
-          <Checkbox
-          className="controllo"
-            label="Alimentazione"
-            value={checkedAlimentazione}
-            onChange={handleChangeAlimentazione} />
-        </div>
-        <div className=" flex">
-          <Checkbox
-          className="controllo"
-            label="Consumi"
-            value={checkedConsumi}
-            onChange={handleChangeConsumi} />
-        </div>
+        { sezioni.map((s) => <Checkbox
+            key={"sezioni-" + s.numero_domande}
+            className="controllo"
+            label={s.titolo}
+            name="sezione"
+            value={s.id_sezione}
+            count={s.numero_domande}
+            checked={s.isChecked}
+            onChange={handleChangeSezione} />)
+        }
       </div>
       <Link
           to="/domande"
